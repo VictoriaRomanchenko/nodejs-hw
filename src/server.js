2795ada66328ba54a5764a1775e024e90b854b36
Victoria Romanchenko <romanchenko.victoria@lll.kpi.ua>
2025-10-26 19:41:09 +0200

    Initial upload to 01-express

--- src/server.js
+++ src/server.js
@@ -1,40 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
-import 'dotenv/config';
-import helmet from 'helmet';
+import pinoHttp from 'pino-http';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(helmet());
+//  Middleware settings
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(express.json());
+app.use(pinoHttp());
 
+//  Routes
+
+//get notes
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
+
 
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
+//test
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
@@ -44,10 +35,14 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
+//err
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  console.error(err.message);
+  res.status(500).json({ message: err.message || 'Internal server error' });
 });
 
+//start
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
